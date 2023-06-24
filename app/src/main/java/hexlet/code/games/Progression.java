4cd9eb90c10f98329a169db4085eea38d62188c2
Владислав Хрусталёв <vlad02041999@yandex.ru>
2023-06-24 16:12:21 +0300

    feat: update Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -28,6 +28,7 @@ public class Progression {
                 startProgression = startProgression + counterOfProgression;
             }
 
+            System.out.print("Question: ");
             int rightAnswer = progressionArr[randomNumber];
             String rigthString = Integer.toString(rightAnswer);
             String progressionString = java.util.Arrays.toString(progressionArr)
